f7b5c3ce606cc5473c69c6755ed8cc2dddced8a0
chanhihi <chanhihi55@gmail.com>
2022-12-21 21:14:39 +0900

    wip: main

--- so_long.c
+++ so_long.c
@@ -6,87 +6,57 @@
 /*   By: chanheki <chanheki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/16 18:54:48 by chanheki          #+#    #+#             */
-/*   Updated: 2022/12/19 22:47:51 by chanheki         ###   ########.fr       */
+/*   Updated: 2022/12/21 20:53:28 by chanheki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./so_long.h"
 
-void	game_init(t_game *game, t_board *board)
+static void	frame_count(t_game *game)
 {
-	board->hei = 0;
-	board->wid = 0;
-	board->map = NULL;
-	board->collectible = 0;
-	board->count_move = 0;
-	board->map_exit = 0;
-	game->board = board;
-	game->mlx = NULL;
-	game->win = NULL;
-	game->tile = NULL;
-	game->wall = NULL;
-	game->cat_down = NULL;
-	game->cat_left = NULL;
-	game->cat_right = NULL;
-	game->cat_up = NULL;
-	game->rat = NULL;
-	game->box_open = NULL;
-	game->box_close = NULL;
+	// int	i;
+
+	game->player->frame++;
+	if (game->player->frame >= PLAYER_MAX_FRAME)
+		game->player->frame = 0;
+	// i = 0;
+	// while (game->enemy[i])
+	// {
+	// 	game->enemy[i]->frame++;
+	// 	if (game->enemy[i]->frame >= ENEMY_MAX_FRAME)
+	// 	game->enemy[i]->frame = 0;
+	// 	i++;
+	// }
+}
+
+static int	main_loop(t_game *game)
+{
+	frame_count(game);
+	draw_player(game, game->player->dir);
+	return (0);
 }
 
 int	main(int argc, char *argv[])
 {
-	t_game	game;
-	t_board	board;
+	t_game		game;
+	t_board		board;
+	t_player	player;
 
 	if (argc == 2)
 	{
 		game_init(&game, &board);
 		map_read(argv[1], &game);
-		game.mlx = mlx_init();
-		game.win = mlx_new_window(game.mlx, game.board->wid * 50, \
-								game.board->hei * 50, "so_long");
-		img_set(&game);
+		so_long_init(&game);
+		player_init(&game, &player);
+		// enemy_init(&game, board.enemy_count);
+		img_init(&game);
 		map_set(&game, 2, game.board->collectible);
-		mlx_key_hook(game.win, key_handler, &game);
+		mlx_hook(game.win, ON_KEYDOWN, 0, key_handler, &game);
 		mlx_hook(game.win, ON_DESTROY, 0, exit_game, &game);
+		mlx_loop_hook(game.mlx, &main_loop, &game);
 		mlx_loop(game.mlx);
 	}
 	else
 		perror ("Usage: ./so_long [MAP_FILE.ber]\n");
 	return (0);
 }
-
-int	exit_game(t_game *game)
-{
-	write(1, "user exit\n", 10);
-	mlx_destroy_window(game->mlx, game->win);
-	exit(0);
-}
-
-int	end_game(t_game *game)
-{
-	print_step_count(game->board->count_move + 1, 0);
-	mlx_destroy_window(game->mlx, game->win);
-	exit(0);
-}
-
-void	ret_error(char *errmsg)
-{
-	write (1, "Error\n", 6);
-	perror (errmsg);
-	exit(0);
-}
-
-void	print_step_count(int count_move, int flag)
-{
-	if (flag)
-		write(1, "step count: ", 12);
-	else
-		write(1, "\ngame over!! step count: ", 25);
-	// ft_putnbr(count_move);
-	ft_putnbr_fd(count_move, 0);
-	write(1, " steps\n", 7);
-}
-
-//arch -x86_64 gcc -L./mlx -lmlx -framework OpenGL -framework AppKit main.c
--- so_long.h
+++ so_long.h
@@ -6,7 +6,7 @@
 /*   By: chanheki <chanheki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/16 18:55:01 by chanheki          #+#    #+#             */
-/*   Updated: 2022/12/19 23:51:05 by chanheki         ###   ########.fr       */
+/*   Updated: 2022/12/21 20:51:15 by chanheki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,48 @@
 # include <stdio.h>
 # include <fcntl.h>
 
+# define PLAYER_MAX_FRAME	30
+# define ENEMY_MAX_FRAME	30
+# define WIDTH				50
+# define HEIGHT				50
+
+# define ASSET_PATH	"./asset"
+
 # define TILE "./asset/tile.xpm"
 # define WALL "./asset/wall.xpm"
-# define RAT "./asset/rat.xpm"
-# define BOX_OPEN "./asset/box_open.xpm"
-# define BOX_CLOSE "./asset/box_close.xpm"
-# define CAT_UP "./asset/cat_up.xpm"
-# define CAT_DOWN "./asset/cat_down.xpm"
-# define CAT_RIGHT "./asset/cat_right.xpm"
-# define CAT_LEFT "./asset/cat_left.xpm"
+# define COLLECT "./asset/collect.xpm"
+# define EXIT_OPEN "./asset/exit_open.xpm"
+# define EXIT_CLOSE "./asset/exit_close.xpm"
+
+# define PLAYER_UP "./asset/player_w00.xpm"
+# define PLAYER_DOWN "./asset/player_s00.xpm"
+# define PLAYER_RIGHT "./asset/player_d00.xpm"
+# define PLAYER_LEFT "./asset/player_a00.xpm"
+
+# define PLAYER_UP_01 "./asset/player_w01.xpm"
+# define PLAYER_DOWN_01 "./asset/player_s01.xpm"
+# define PLAYER_RIGHT_01 "./asset/player_d01.xpm"
+# define PLAYER_LEFT_01 "./asset/player_a01.xpm"
+
+# define PLAYER_UP_02 "./asset/player_w02.xpm"
+# define PLAYER_DOWN_02 "./asset/player_s02.xpm"
+# define PLAYER_RIGHT_02 "./asset/player_d02.xpm"
+# define PLAYER_LEFT_02 "./asset/player_a02.xpm"
+
+# define ENEMY_UP "./asset/enemy_w00.xpm"
+# define ENEMY_DOWN "./asset/enemy_s00.xpm"
+# define ENEMY_RIGHT "./asset/enemy_d00.xpm"
+# define ENEMY_LEFT "./asset/enemy_a00.xpm"
+
+# define ENEMY_UP_01 "./asset/enemy_w01.xpm"
+# define ENEMY_DOWN_01 "./asset/enemy_s01.xpm"
+# define ENEMY_RIGHT_01 "./asset/enemy_d01.xpm"
+# define ENEMY_LEFT_01 "./asset/enemy_a01.xpm"
+
+# define ENEMY_UP_02 "./asset/enemy_w02.xpm"
+# define ENEMY_DOWN_02 "./asset/enemy_s02.xpm"
+# define ENEMY_RIGHT_02 "./asset/enemy_d02.xpm"
+# define ENEMY_LEFT_02 "./asset/enemy_a02.xpm"
 
 /* mlx_event */
 typedef enum e_mlx_event {
@@ -71,24 +104,48 @@ typedef struct s_board
 	int		collectible;
 	int		count_move;
 	int		map_exit;
+	int		enemy_count;
 }	t_board;
+
+/* player */
+typedef struct s_player
+{
+	int		frame;
+	int		dir;
+	int		x;
+	int		y;
+	void	*player_up[3];
+	void	*player_down[3];
+	void	*player_right[3];
+	void	*player_left[3];
+}	t_player;
+
+typedef struct s_enemy
+{
+	int		frame;
+	int		type;
+	int		dir;
+	int		x;
+	int		y;
+	void	*enemy_up[3];
+	void	*enemy_down[3];
+	void	*enemy_right[3];
+	void	*enemy_left[3];
+}	t_enemy;
 
 /* so_long game */
 typedef struct s_game
 {
-	t_board	*board;
-
-	void	*mlx;
-	void	*win;
-	void	*tile;
-	void	*wall;
-	void	*cat_up;
-	void	*cat_down;
-	void	*cat_right;
-	void	*cat_left;
-	void	*rat;
-	void	*box_close;
-	void	*box_open;
+	t_board		*board;
+	t_player	*player;
+	t_enemy		**enemy;
+	void		*mlx;
+	void		*win;
+	void		*tile;
+	void		*wall;
+	void		*collect;
+	void		*exit_close;
+	void		*exit_open;
 }	t_game;
 
 /* so_long func */
@@ -100,11 +157,20 @@ void	print_step_count(int count_move, int flag);
 
 void	check_validate_board(t_game *game, char *str);
 
-void	img_set(t_game *game);
-void	map_set(t_game *game, int pressed_key, int collectible);
+void	img_init(t_game *game);
+void	map_set(t_game *game, int key_code, int collectible);
 void	map_read(char *filename, t_game *game);
 void	put_box_image(t_game *g, int hei, int wid, int collectible);
-void	put_cat_image(t_game *game, int height, int width, int key_code);
+void	draw_player(t_game *game, int key_code);
 
+void	player_position(t_game *game);
 int		key_handler(int key, t_game *game);
+
+void	so_long_init(t_game *game);
+void	game_init(t_game *game, t_board *board);
+void	player_init(t_game *game, t_player *player);
+void	enemy_init(t_game *game, int enemy_count);
+
+void	*mxfti(void *xvar, char *file, int *width, int *height);
+int		mpitw(t_game *game, void *img_ptr, int x, int y);
 #endif
