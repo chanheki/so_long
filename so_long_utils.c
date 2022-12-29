2599e7c10066885521804c27034f517cb3e2c902
chanhihi <chanhihi55@gmail.com>
2022-12-29 21:21:34 +0900

    feat: lose game

--- so_long_utils.c
+++ so_long_utils.c
@@ -15,8 +15,22 @@
 int	exit_game(t_game *game)
 {
 	write(1, "user exit\n", 10);
+	write(1, "\nGame Win!! ", 12);
 	mlx_destroy_window(game->mlx, game->win);
 	exit(0);
+}
+
+void	lose_game(t_game *game)
+{
+	game->game_status = GAME_OVER;
+	draw_player_lose(game);
+	print_step_count(game->board->count_move + 1, 0);
+	write (1, "lose game\n", 10);
+	if (game->game_status == SHUTDOWN)
+	{
+		mlx_destroy_window(game->mlx, game->win);
+		exit(0);
+	}
 }
 
 int	end_game(t_game *game)
@@ -38,7 +52,7 @@ void	print_step_count(int count_move, int flag)
 	if (flag)
 		write(1, "step count: ", 12);
 	else
-		write(1, "\ngame over!! step count: ", 25);
+		write(1, "step count: ", 13);
 	ft_putnbr_fd(count_move, 0);
 	write(1, " steps\n", 7);
 }
